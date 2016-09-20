23e92c0fb6d136a05adb208a237f93a10b14a96e
Édouard Lopez <edouard@akema.fr>
2016-09-20 11:35:31 +0200

    link navbar to sections

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -2,14 +2,11 @@
 	<nav class="navbar navbar-full navbar-dark bg-inverse">
 		<ul class="nav navbar-nav">
 			<a class="navbar-brand" href="#">Apidae</a>
-			<li class="nav-item active">
-				<a class="nav-link" href="#">Accueil<span class="sr-only">(current)</span></a>
+			<li class="nav-item">
+				<a class="nav-link" href="#fonctionnalités">Solutions</a>
 			</li>
 			<li class="nav-item">
-				<a class="nav-link" href="#">Solutions</a>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link" href="#">Contact</a>
+				<a class="nav-link" href="#nous-contacter">Contact</a>
 			</li>
 		</ul>
 	</nav>
