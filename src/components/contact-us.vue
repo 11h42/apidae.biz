e9238019bfa28a9cf2a120877c1053d33121552f
Édouard Lopez <edouard@akema.fr>
2016-09-20 17:23:54 +0200

    fix typos

--- src/components/contact-us.vue
+++ src/components/contact-us.vue
@@ -3,13 +3,13 @@
 		<h3>Contactez-nous</h3>
 		<ul class="vcard list-unstyled">
 			<li class="tel">
-				<i class="fa fa-phone" aria-hidden="true" title="Téléphone"></i> :
-				<abbr title="téléphone">Tél.</abbr>:
+				<i class="fa fa-phone" aria-hidden="true" title="Téléphone"></i>
+				<abbr title="téléphone">tél.</abbr>:
 				<span class="value">(+33).556.787.250</span>
 			</li>
 			<li class="email">
-				<i class="fa fa-envelope" aria-hidden="true" title="par e-mail"></i> :
-				<abbr title="courriel">email</abbr>:
+				<i class="fa fa-envelope" aria-hidden="true" title="par e-mail"></i>
+				<abbr title="courriel">e-mail</abbr>:
 				<span class="value">
 				<a href="mailto:apidae@akema.fr">apidae@akema.fr</a>.
 				</span>
--- src/components/our-services.vue
+++ src/components/our-services.vue
@@ -4,7 +4,7 @@
 		<ul class="list-unstyled">
 			<li>
 				<i class="fa fa-envelope-o" aria-hidden="true" ></i>
-				<a href="akemail.fr">Akemail</a>, solution d’emailing ;</li>
+				<a href="akemail.fr">Akemail</a>, solution d’e-mailing ;</li>
 			<li>
 				<i class="fa fa-clock-o" aria-hidden="true" ></i>
 				<a href="11h42.fr">11h42</a>, accélérateur de projet.</li>
