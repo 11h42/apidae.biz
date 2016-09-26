34bee9e344e28f6066a2f905f7d72cf4b6163b5c
Édouard Lopez <edouard@akema.fr>
2016-09-26 16:39:41 +0200

    fix services URLs
    fixes akema/gestion#138

--- src/components/our-services.vue
+++ src/components/our-services.vue
@@ -4,10 +4,10 @@
 		<ul class="list-unstyled">
 			<li>
 				<i class="fa fa-envelope-o" aria-hidden="true" ></i>
-				<a href="akemail.fr">Akemail</a>, solution d’e-mailing ;</li>
+				<a href="http://akemail.fr">Akemail</a>, solution d’e-mailing ;</li>
 			<li>
 				<i class="fa fa-clock-o" aria-hidden="true" ></i>
-				<a href="11h42.fr">11h42</a>, accélérateur de projet.</li>
+				<a href="http://11h42.com">11h42</a>, accélérateur de projet.</li>
 		</ul>
 	</section>
 </template>
