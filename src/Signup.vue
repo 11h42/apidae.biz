1e7c919b497937728e187d70dbb4580f978b0c9a
mdubourg <mdubourg@akema.fr>
2018-11-05 16:44:59 +0100

    added spinner on form submission

--- src/Signup.vue
+++ src/Signup.vue
@@ -3,7 +3,7 @@
 
 		<h2 class="font-weight-bold">Essayer gratuitement</h2>
 		<hr>
-		<h5 class="text-muted">Découvrez les fonctionnalités d'Apidae<br />
+		<h5 class="text-muted">Découvrez les fonctionnalités d'Apidae<br/>
 			<span class="text-warning">pendant deux mois gratuitement et sans engagement</span> !</h5>
 
 		<div class="row">
@@ -33,16 +33,26 @@
 					<input type="text" name="telephone" id="telephone" class="margin-bottom" v-model="form.telephone"
 						   :disabled="submit_success">
 
-                    <label for="password">Mot de passe* : <i class="text-muted">(pourquoi ne pas utiliser <a
-                            target="_blank" href="https://lesspass.com/">LessPass</a> ?)</i></label>
-                    <input type="password" name="password" id="password" class="margin-bottom" v-model="form.password" :disabled="submit_success">
-                    
+					<label for="password">Mot de passe* : <i class="text-muted">(pourquoi ne pas utiliser <a
+							target="_blank" href="https://lesspass.com/">LessPass</a> ?)</i></label>
+					<input type="password" name="password" id="password" class="margin-bottom" v-model="form.password"
+						   :disabled="submit_success">
+
 					<br/>
-					<br/>          
-          <div class="g-recaptcha" data-sitekey="6Lcvy0oUAAAAAHDt9ZyGMBTWSRPsMiM58aZCg4gR"></div>
-          <br />
+					<br/>
+					<div class="g-recaptcha" data-sitekey="6Lcvy0oUAAAAAHDt9ZyGMBTWSRPsMiM58aZCg4gR"></div>
+					<br/>
 					<i v-if="error_message" id="error-message">{{ error_message }}</i>
-					<input type="submit" value="Valider" class="btn btn-lg">
+					<input v-if="!loading" type="submit" value="Valider" class="btn btn-lg">
+					<button v-if="loading" class="btn btn-lg" id="form-loading-button">
+						<div class="lds-ring">
+							<div></div>
+							<div></div>
+							<div></div>
+							<div></div>
+						</div>
+						<span>Inscription en cours</span>
+					</button>
 				</form>
 			</div>
 
@@ -62,147 +72,203 @@
 				<br/>
 				<h3>Nous vous avons envoyé un mail, </h3>
 				<h5><b>cliquez sur le lien à l'intérieur de celui-ci pour activer votre compte !</b>
-            <br />
-            <br />
-            <small>Attention, vous ne pourrez pas vous connecter avant d'avoir validé votre e-mail !</small>
-        </h5>
-				<br/>
-				<a :href="'https://' + login_url" class="btn">Accéder à Apidae</a>
+					<br/>
+					<br/>
+					<small>Attention, vous ne pourrez pas vous connecter avant d'avoir validé votre e-mail !</small>
+				</h5>
 			</div>
 		</div>
 	</div>
 </template>
 <script type="text/ecmascript-6">
-import Navbar from "./components/navbar";
-import MyFooter from "./components/footer";
+	import Navbar from "./components/navbar";
+	import MyFooter from "./components/footer";
 
-export default {
-  components: {
-    Navbar,
-    MyFooter
-  },
-  data() {
-    return {
-      form: {},
-      error_message: "",
-      submit_success: false,
-      login_url: ""
-    };
-  },
-  methods: {
-    send_form: function() {
-      let _this = this;
-      this.form.gRecaptchaResponse = grecaptcha.getResponse();
-      $.post({
-        url: "https://apidae-dev.akema.fr/api/signup/",
-        crossDomain: true,
-        dataType: "text",
-        contentType: "application/x-www-form-urlencoded",
-        data: this.form
-      })
-        .done(function(data) {
-          _this.submit_success = true;
-          _this.login_url = data;
-          $("#inner-form").slideUp("slow");
-        })
-        .fail(function(error) {
-          _this.error_message = error.responseText;
-        });
-    }
-  }
-};
+	export default {
+		components: {
+			Navbar,
+			MyFooter
+		},
+		data() {
+			return {
+				form: {},
+				error_message: "",
+				submit_success: false,
+				login_url: "",
+				loading: false
+			};
+		},
+		methods: {
+			send_form: function () {
+				let _this = this;
+				this.loading = true;
+				this.form.gRecaptchaResponse = grecaptcha.getResponse();
+				$.post({
+					url: "https://apidae-dev.akema.fr/api/signup/",
+					crossDomain: true,
+					dataType: "text",
+					contentType: "application/x-www-form-urlencoded",
+					data: this.form
+				})
+						.done(function (data) {
+							_this.submit_success = true;
+							_this.login_url = data;
+							_this.loading = false;
+							$("#inner-form").slideUp("slow");
+						})
+						.fail(function (error) {
+							_this.error_message = error.responseText;
+							_this.loading = false;
+						});
+			}
+		}
+	};
 </script>
 
 <style>
-@import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-@import "../node_modules/font-awesome/css/font-awesome.min.css";
-@import "../node_modules/hint.css/hint.min.css";
+	@import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+	@import "../node_modules/font-awesome/css/font-awesome.min.css";
+	@import "../node_modules/hint.css/hint.min.css";
 
-#signup-form {
-  padding: 35px 15px;
-  text-align: center;
-}
+	#signup-form {
+		padding: 35px 15px;
+		text-align: center;
+	}
 
-#inner-form {
-  text-align: left;
-  margin: 50px 0 50px 0;
-  padding: 20px 30px 20px 30px;
-  box-shadow: 0 15px 30px 0 rgba(0,0,0,0.11),
-            0 5px 15px 0 rgba(0,0,0,0.08);
-  transition: opacity 1s ease;
-}
+	#inner-form {
+		text-align: left;
+		margin: 50px 0 50px 0;
+		padding: 20px 30px 20px 30px;
+		box-shadow: 0 15px 30px 0 rgba(0, 0, 0, 0.11),
+		0 5px 15px 0 rgba(0, 0, 0, 0.08);
+		transition: opacity 1s ease;
+	}
 
-#inner-form.faded {
-  opacity: 0.6;
-}
+	#inner-form.faded {
+		opacity: 0.6;
+	}
 
-#signup-form form {
-  margin-bottom: 50px;
-}
+	#signup-form form {
+		margin-bottom: 50px;
+	}
 
-#signup-form form label {
-  color: #3c3c3c;
-  font-weight: bold;
-  font-size: 13px;
-}
+	#signup-form form label {
+		color: #3c3c3c;
+		font-weight: bold;
+		font-size: 13px;
+	}
 
-#signup-form form label a {
-  color: #0275d8 !important;
-}
+	#signup-form form label a {
+		color: #0275d8 !important;
+	}
 
-#signup-form form input {
-  background-color: white;
-  color: #3c3c3c;
-  outline: none;
-  border: 1px solid rgba(142, 153, 138, 0.5);
-  padding: 4px 8px;
-  width: 100%;
-}
+	#signup-form form input {
+		background-color: white;
+		color: #3c3c3c;
+		outline: none;
+		border: 1px solid rgba(142, 153, 138, 0.5);
+		padding: 4px 8px;
+		width: 100%;
+	}
 
-#signup-form form .margin-bottom {
-  margin-bottom: 8px;
-}
+	#signup-form form .margin-bottom {
+		margin-bottom: 8px;
+	}
 
-#signup-form form .btn {
-  background-color: #f4511e;
-  color: white;
-  width: fit-content;
-  width: -moz-fit-content;
-  padding: 7px 20px;
-  border: none;
-  float: right;
-  margin-top: 15px;
-}
+	#signup-form form .btn {
+		background-color: #f4511e;
+		color: white;
+		width: fit-content;
+		width: -moz-fit-content;
+		padding: 7px 20px;
+		border: none;
+		float: right;
+		margin-top: 15px;
+	}
 
-#access-pricing-div {
-  text-align: center;
-  padding: 200px 50px;
-}
+	#access-pricing-div {
+		text-align: center;
+		padding: 200px 50px;
+	}
 
-#access-pricing-div .btn {
-  background-color: #1b6d85;
-  padding-left: 20px;
-  padding-right: 20px;
-}
+	#access-pricing-div .btn {
+		background-color: #1b6d85;
+		padding-left: 20px;
+		padding-right: 20px;
+	}
 
-#submit-success-div {
-  text-align: center;
-  padding: 200px 50px;
-  opacity: 0;
-  transition: opacity 1s;
-}
+	#submit-success-div {
+		text-align: center;
+		padding: 200px 50px;
+		opacity: 0;
+		transition: opacity 1s;
+	}
 
-#submit-success-div.visible {
-  opacity: 1;
-}
+	#submit-success-div.visible {
+		opacity: 1;
+	}
 
-#submit-success-div .btn {
-  background-color: #f4511e;
-  padding-left: 35px;
-  padding-right: 35px;
-}
+	#submit-success-div .btn {
+		background-color: #f4511e;
+		padding-left: 35px;
+		padding-right: 35px;
+	}
 
-#error-message {
-  color: rgb(255, 0, 0);
-}
+	#error-message {
+		color: rgb(255, 0, 0);
+	}
+
+	/* ======================== */
+	/* loading animations style */
+
+	#form-loading-button {
+		display: flex;
+		align-items: center;
+	}
+
+	.lds-ring {
+		display: inline-block;
+		position: relative;
+		top: -3px;
+		left: -5px;
+		width: 32px;
+		height: 32px;
+		margin-right: 10px;
+	}
+
+	.lds-ring div {
+		box-sizing: border-box;
+		display: block;
+		position: absolute;
+		width: 24px;
+		height: 24px;
+		margin: 6px;
+		border: 3px solid #fff;
+		border-radius: 50%;
+		animation: lds-ring 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+		border-color: #fff transparent transparent transparent;
+	}
+
+	.lds-ring div:nth-child(1) {
+		animation-delay: -0.45s;
+	}
+
+	.lds-ring div:nth-child(2) {
+		animation-delay: -0.3s;
+	}
+
+	.lds-ring div:nth-child(3) {
+		animation-delay: -0.15s;
+	}
+
+	@keyframes lds-ring {
+		0% {
+			transform: rotate(0deg);
+		}
+		100% {
+			transform: rotate(360deg);
+		}
+	}
+
+
 </style>
