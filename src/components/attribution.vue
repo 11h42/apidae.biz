4048b73e1460f4e065ea5799346d6c26aa454c88
Catherine Timonnier <temadia@hotmail.fr>
2019-02-05 11:29:31 +0100

    Update attribution.vue

--- src/components/attribution.vue
+++ src/components/attribution.vue
@@ -1,7 +1,7 @@
 <template>
 	<div id="attribution" class="m-x-auto text-xs-center">
 		<p class="text-muted">
-			© 2017 Apidae
+			© 2019 Apidae
 			·
 			<a href="/src/mentions-legales.html">Mentions légales</a>
 			par <a href="http://akema.fr">Akema.fr</a>.
