5d6b0721eed852ceed3ad2c1cd2cb52a72d010e4
Jeremi Manenti <jeremi.manenti@carestream.com>
2019-04-04 17:51:37 +0200

    Update navbar.vue
    change menu to the mockup marie

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,11 +1,21 @@
 <template>
-	<nav class="navbar navbar-full bg-inverse">
-		<ul class="nav navbar-nav container d-flex flex-row">
+	<nav class="navbar navbar-full navbar-dark">
+		<div class="container">
 			<a class="navbar-brand" href="/">
 				<img src="../assets/logo-apidae.png" alt="Logo Apidae" id="navbar-logo">
 				&nbsp;Apidae</a>
+			<div class="nav-item navmenu" id="navmenuId">
+				<a href="/">Accueil</a>
+				<a href="#">Suivi d'activité</a>
+				<a href="#">Congés et absences</a>
+				<a href="#">Notes de frais</a>
+				<a href="#">Time tracking</a>
+				<a href="javascript:void(0);" class="icon" @click="transformMenu()">
+					<i class="fa fa-bars"></i>
+				</a>
+			</div>
 			<a class="nav-item btn btn-blue" v-if="showbtntry" @click="scroll_to_signup">Essai gratuit</a>
-		</ul>
+		</div>
 	</nav>
 </template>
 <script type="text/ecmascript-6">
@@ -15,6 +25,16 @@
 			showbtntry: {type: Boolean, required: true},
 			scroll_to_signup: {type: Function, required: true}
 		},
+		methods: {
+			transformMenu: function () {
+				var x = document.getElementById("navmenuId");
+				if (x.className === "navmenu") {
+					x.className += " responsive";
+				} else {
+					x.className = "navmenu";
+				}
+			}
+		}
 	}
 </script>
 <style>
@@ -36,16 +56,55 @@
 
 	nav a.btn {
 		float: right !important;
-		margin-top: 15px;
+		margin-top: 0px;
 		margin-right: 15px;
 	}
 
 	nav a.btn-blue {
-		background-color: #1B6D85;
+		background-color: #fcce21;
 	}
 
 	nav a.btn-default {
 		border: 1px solid white;
+	}
+
+	.navmenu {
+	  	overflow: hidden;
+		margin-top: 1em;
+	}
+
+	.navmenu a {
+	  	float: left;
+	  	display: block;
+	  	text-align: center;
+	  	padding: 7px 8px;
+	  	font-size: 1.3em;
+	}
+
+	.navmenu .icon {
+	  display: none;
+	}
+
+	@media screen and (max-width: 62em) {
+	  .navmenu a {display: none;}
+	  .navmenu a.icon {
+		float: right;
+		display: block;
+	  }
+	}
+
+	@media screen and (max-width: 62em) {
+	  .navmenu.responsive {position: relative;}
+	  .navmenu.responsive .icon {
+		position: absolute;
+		right: 0;
+		top: 0;
+	  }
+	  .navmenu.responsive a {
+		float: none;
+		display: block;
+		text-align: left;
+	  }
 	}
 
 </style>
