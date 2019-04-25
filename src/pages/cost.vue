b5af30768c1e65ed23f097d3a7492cda74deeb88
Jeremi Manenti <jeremi.manenti@carestream.com>
2019-04-25 15:16:59 +0200

    add the more commun element

--- src/pages/cost.vue
+++ src/pages/cost.vue
@@ -1,13 +1,42 @@
 <template>
     <div>
+      <pricing :ascomponent="true" :scroll_to_signup="scroll_to_signup"></pricing>
 
+      <signup :scroll_to_price="scroll_to_price"></signup>
+
+      <section id="nous-contacter" class="row default-container">
+        <call-to-action class="col-xs-12"></call-to-action>
+      </section>
     </div>
 </template>
 
-<script>
-    export default {
-        name: "cost"
+<script type="text/ecmascript-6">
+  import Cover from "./../components/cover";
+  import Argument from "./../components/argument";
+  import AdvantageUsers from "./../components/advantage-users";
+  import CallToAction from "./../components/call-to-action";
+  import Pricing from "./../Pricing.vue";
+  import Signup from "./../Signup.vue";
+
+  export default {
+    props: {
+      scroll_to_signup: { type: Function, required: true },
+      scroll_to_price: { type: Function, required: true },
+      change_page_selected: { type: Function, required: true }
+    },
+
+    components: {
+      Cover,
+      Argument,
+      AdvantageUsers,
+      CallToAction,
+      Pricing,
+      Signup
+    },
+
+    methods: {
     }
+  };
 </script>
 
 <style scoped>
