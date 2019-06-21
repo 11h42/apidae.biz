29778ed4cf15a539c61e91a001292ed845172728
jeremi33 <jmanenti@akema.fr>
2019-06-21 09:46:21 +0200

    upper case

--- src/components/our-address.vue
+++ src/components/our-address.vue
@@ -6,7 +6,7 @@
       <span class="fn org name">Apidae - AKEMA SAS</span> <br>
       <a href="https://goo.gl/maps/zaEWdjBGvh72">
         <address class="adr">
-          <span class="">bâtiment B </span><br>
+          <span class="">Bâtiment B </span><br>
           <span class="street-address">4 Voie Romaine 33610</span><br>
           <span class="postal-code">33610</span>
           <span class="locality">Canéjan</span>,<br>
