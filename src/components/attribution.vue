308e1456acdf3c81332bf134d891d2108211ac98
benjaminduarte <70194665+benjaminduarte@users.noreply.github.com>
2021-04-26 08:41:00 +0200

    Update attribution.vue

--- src/components/attribution.vue
+++ src/components/attribution.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="attribution" class="m-x-auto text-xs-center">
     <p class="text-muted">
-      © 2020 Apidae
+      © 2021 Apidae
       ·
       <a><router-link to="/mentions-legales">Mentions légales</router-link></a>
       par <a href="http://akema.fr">Akema.fr</a>.
